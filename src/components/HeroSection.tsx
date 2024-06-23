fa41ac8db92ed29b0a216672c0c40261950445dc
Daniyal <dani.play49@gmail.com>
2024-06-23 11:46:52 -0700

    ssvd

--- src/components/HeroSection.tsx
+++ src/components/HeroSection.tsx
@@ -6,18 +6,18 @@ import { Button } from "./ui/moving-border";
 const HeroSection = () => {
     return (
         <>
-            <Spotlight
-                className="-top-40 left-0 md:left-60 md:-top-20"
-                fill="white"
-            />
             <div className='h-auto md:h-[40rem] w-full rounded-md flex flex-col justify-center items-center relative
     overflow-hidden mx-auto py-10 md:py-0'>
+        <Spotlight
+            className="-top-40 left-0 md:left-60 md:-top-20"
+            fill="white"
+        />
                 <div className="p-4 relative z-10 w-full text-center">
                     <h1 className="mt-20 md:mt-0 text-4xl md:text-7xl font-bold bg-clip-text text-transparent bg-gradient-to-b from-neutral-50 to-neutral-400">Master the art of music</h1>
                     <p className="mt-4 font-normal text-base md:text-lg text-neutral-300 max-w-lg mx-auto">Lorem ipsum dolor sit amet consectetur, adipisicing elit. Omnis rem eaque veritatis totam deleniti aperiam, maxime assumenda soluta officia odit. Lorem ipsum dolor sit amet consectetur, adipisicing elit. Aperiam, dicta.</p>
                     <div className="mt-4">
                         <Link href={'/courses'}>
-                            <Button   borderRadius="1.75rem">
+                            <Button className='bg-dark'  borderRadius="1.75rem">
                                   Explore Courses
                             </Button>
                         </Link>
