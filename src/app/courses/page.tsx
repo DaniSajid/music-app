3dc72892ea3042be96c9101af3bd53704cf7d1ea
Daniyal <dani.play49@gmail.com>
2024-06-24 00:02:10 -0700

    updatenewdani

---
+++ src/app/courses/page.tsx
@@ -0,0 +1,16 @@
+"use client";
+ 
+import Image from "next/image";
+import React from "react";
+import { CardBody, CardContainer, CardItem } from "../ui/3d-card";
+import Link from "next/link";
+
+const page = () => {
+  return (
+    <div>
+      
+    </div>
+  )
+}
+
+export default page
